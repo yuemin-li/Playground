28d4c6f763bd1520e092455d5030bf85e93ad488
Yuemin Li <yuemin-li@github.com>
2015-08-06 16:21:49 -0400

    reconstruct

--- identity.go
+++ identity.go
@@ -1,43 +1,23 @@
-package main
+package identity
 
 import (
     "fmt"
     "net/http"
-    "io/ioutil"
-    //"github.com/go-martini/martini"
 )
 
-const AUTH_POINT = "https://staging.identity.api.rackspacecloud.com/v2.0/tokens"
-
-func main() {
-    token := "AADR4CYXAu4zFye0t5j5DieNzQDrRYev35c26HTYP00J8kQYgZ8_6BMPXtpzv59rReJO0jO_xq-4OwPbNgmbINT0oaZQLfayb_1jYezECd4Z4Scle_LGXkHD"
-
-    req, err := http.NewRequest("GET", AUTH_POINT+"/"+token, nil)
-    req.Header.Set("X-Auth-Token", token+"A")
-
+func Auth(token, endpoint string) int {
+    req, err := http.NewRequest("GET", endpoint+"/"+token, nil)
+    req.Header.Set("X-Auth-Token", token)
+    
     client := &http.Client{}
     resp, err := client.Do(req)
+    RespCode := resp.StatusCode
+    
     if err != nil {
-        fmt.Println("Request has error")
+        fmt.Println("Error communicating with identity.")
         panic(err)
     }
-    if resp.Status != "200 OK" {
-        fmt.Println(resp.Status)
-        panic("not autherized.")
-    }
-
-    defer resp.Body.Close()
-
-    fmt.Println("response Status:", resp.Status)
-    fmt.Println("response Headers:", resp.Header)
-    body, _ := ioutil.ReadAll(resp.Body)
-    fmt.Println("response Body:", string(body))
-    fmt.Println(resp)
-    fmt.Println(err)
+    defer resp.Body.Close() 
+    return RespCode
+    
 }
-
-// func main(){
-//     m := martini.Classic()
-//     m.Use(auth.Token("TokenFoundInCache"))
-//     m.Run()
-// }
